6dae165e7a5ed9256bb51b4615a63c0862cd2794
milktrader <milktrader@gmail.com>
2013-01-15 13:43:29 -0500

    added bee.r

---
+++ demo/bee.r
@@ -0,0 +1,139 @@
+#!/usr/bin/Rscript --vanilla
+#
+# Bumblebee trading system
+# copyright (c) 2009-2013, Algorithm Alpha, LLC
+# Licensed GPL-2
+
+############################### REQUIRE ####################################
+
+require(quantstrat)
+
+############################# GET DATA ######################################
+
+data(spx)
+
+############################# DEFINE VARIABLES ##############################
+
+port          = 'bug'
+acct          = 'colony'
+initEq        = 100000
+initDate      = '1999-12-31'
+fast          = 10
+slow          = 30
+sd            = 0.5
+
+############################# INITIALIZE ####################################
+
+currency('USD')
+stock('spx',currency='USD', multiplier=1)
+initPortf(port, 'spx', initDate=initDate)
+initAcct(acct, port, initEq=initEq, initDate=initDate)
+initOrders(port, initDate=initDate )
+bee = strategy(port)
+
+############################# MAX POSITION LOGIC ############################
+
+addPosLimit(
+            portfolio=port,
+            symbol='spx', 
+            timestamp=initDate,  
+            maxpos=100)
+
+
+############################# INDICATORS ####################################
+
+bee <- add.indicator( 
+                     strategy  = bee, 
+                     name      = 'BBands', 
+                     arguments = list(HLC=quote(HLC(mktdata)), 
+                                      n=slow, 
+                                      sd=sd))
+
+bee <- add.indicator(
+                     strategy  = bee, 
+                     name      = 'SMA', 
+                     arguments = list(x=quote(Cl(mktdata)), 
+                                      n=fast),
+                     label     = 'fast' )
+
+############################# SIGNALS #######################################
+
+bee <- add.signal(
+                  strategy  = bee,
+                  name      = 'sigCrossover',
+                  arguments = list(columns=c('fast','dn'), 
+                                   relationship='lt'),
+                  label     = 'fast.lt.dn')
+
+bee <- add.signal(
+                  strategy  = bee,
+                  name      = 'sigCrossover',
+                  arguments = list(columns=c('fast','up'),
+                                   relationship='gt'),
+                  label     = 'fast.gt.up')
+
+############################# RULES #########################################
+
+bee <- add.rule(
+                strategy  = bee,
+                name      = 'ruleSignal',
+                arguments = list(sigcol    = 'fast.gt.up',
+                                 sigval    = TRUE,
+                                 orderqty  = 100,
+                                 ordertype = 'market',
+                                 orderside = 'long',
+                                 osFUN     = 'osMaxPos'),
+
+                type      = 'enter',
+                label     = 'EnterLONG')
+
+bee <- add.rule(
+                strategy  = bee,
+                name      = 'ruleSignal',
+                arguments = list(sigcol    = 'fast.lt.dn',
+                                 sigval    = TRUE,
+                                 orderqty  = 'all',
+                                 ordertype = 'market',
+                                 orderside = 'long'),
+                type      = 'exit',
+                label     = 'ExitLONG')
+
+bee <- add.rule(
+                strategy  = bee,
+                name      = 'ruleSignal',
+                arguments = list(sigcol     = 'fast.lt.dn',
+                                  sigval    = TRUE,
+                                  orderqty  =  -100,
+                                  ordertype = 'market',
+                                  orderside = 'short',
+                                  osFUN     = 'osMaxPos'),
+                type      = 'enter',
+                label     = 'EnterSHORT')
+
+bee <- add.rule(
+                strategy  = bee,
+                name      = 'ruleSignal',
+                arguments = list(sigcol     = 'fast.gt.up',
+                                 sigval     = TRUE,
+                                 orderqty   = 'all',
+                                 ordertype  = 'market',
+                                 orderside  = 'short'),
+                type      = 'exit',
+                label     = 'ExitSHORT')
+
+############################# APPLY STRATEGY ################################
+
+applyStrategy(bee, port, prefer='Open', verbose=FALSE)
+
+############################# UPDATE ########################################
+
+updatePortf(port, 'spx', Date=paste('::',as.Date(Sys.time()),sep=''))
+updateAcct(acct)
+
+##################### CONTAINERS CALLED IN TESTING #####################
+                                                               #########
+rets  = PortfReturns(acct)                                     #########
+book  = getOrderBook(port)                                     #########
+stats = tradeStats(port)                                       #########
+                                                               #########
+########################################################################
